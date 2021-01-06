ac5a22cdb5fdc8fef03101f0cbe835b04fad0057
Rebekah Kahn <rebekah.m.kahn@gmail.com>
2021-01-06 14:04:22 -0500

    activity indicator

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { FlatList } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -11,13 +12,13 @@ const mapStateToProps = state => {
 };
 
 class Directory extends Component {
-
     static navigationOptions = {
         title: 'Directory'
     }
 
     render() {
         const { navigate } = this.props.navigation;
+        
         const renderDirectoryItem = ({item}) => {
             return (
                 <Tile
@@ -29,6 +30,18 @@ class Directory extends Component {
                 />
             );
         };
+
+        if (this.props.campsites.isLoading) {
+            return <Loading />;
+        }
+        
+        if (this.props.campsites.errMess) {
+            return (
+                <View>
+                    <Text>{this.props.campsites.errMess}</Text>
+                </View>
+            );
+        }
 
         return (
             <FlatList
